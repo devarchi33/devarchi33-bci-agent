f3d43e401e858cb37cb323c2f0eaf811160a1353
devarchi33 <devarchi33@gmail.com>
2016-04-22 08:14:08 +0900

    add test module.

--- HelloBci/src/HelloBci.java
+++ HelloBci/src/HelloBci.java
@@ -2,4 +2,12 @@
  * Created by donghoon on 2016. 4. 22..
  */
 public class HelloBci {
+    public static void main(String[] args) {
+        HelloBci hb = new HelloBci();
+        hb.doSomething();
+    }
+
+    void doSomething() {
+        System.out.println("Hello Bci!");
+    }
 }
