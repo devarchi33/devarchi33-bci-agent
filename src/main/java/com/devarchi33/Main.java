d7e138783aa2fbdbc36db79c7d6f0b6211ab05a6
devarchi33 <devarchi33@gmail.com>
2016-04-22 07:19:19 +0900

    first commit, add main class

---
+++ src/main/java/com/devarchi33/Main.java
@@ -0,0 +1,14 @@
+package com.devarchi33;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        Main main = new Main();
+        main.doSomething();
+    }
+
+    void doSomething() {
+        System.out.println("do something...");
+    }
+}
